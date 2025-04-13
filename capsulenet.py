06e266282a54e3714de743219e89a0ce5699291a
vladaunski <v.barbulescu@student.tue.nl>
2025-04-13 18:51:45 +0200

    Some prototypes for the capsnet model

---
+++ capsulenet.py
@@ -0,0 +1,228 @@
+"""
+Keras implementation of CapsNet in Hinton's paper Dynamic Routing Between Capsules.
+The current version maybe only works for TensorFlow backend. Actually it will be straightforward to re-write to TF code.
+Adopting to other backends should be easy, but I have not tested this. 
+
+Usage:
+       python capsulenet.py
+       python capsulenet.py --epochs 50
+       python capsulenet.py --epochs 50 --routings 3
+       ... ...
+    
+"""
+
+import numpy as np
+import tensorflow as tf
+import pandas as pd
+from tensorflow.keras import layers, models, optimizers
+from tensorflow.keras import backend as K
+from tensorflow.keras.utils import to_categorical
+import matplotlib.pyplot as plt
+from PIL import Image
+from capsulelayers import CapsuleLayer, PrimaryCap, Length, Mask
+
+K.set_image_data_format('channels_last')
+
+def CapsNet(input_shape, n_class, routings, batch_size):
+    """
+    A Capsule Network on MNIST.
+    :param input_shape: data shape, 3d, [width, height, channels]
+    :param n_class: number of classes
+    :param routings: number of routing iterations
+    :param batch_size: size of batch
+    :return: Two Keras Models, the first one used for training, and the second one for evaluation.
+            `eval_model` can also be used for training.
+    """
+    x = layers.Input(shape=input_shape, batch_size=batch_size)
+
+    # Layer 1: Just a conventional Conv2D layer
+    conv1 = layers.Conv2D(filters=256, kernel_size=9, strides=1, padding='valid', activation='relu', name='conv1')(x)
+
+    # Layer 2: Conv2D layer with `squash` activation, then reshape to [None, num_capsule, dim_capsule]
+    primarycaps = PrimaryCap(conv1, dim_capsule=8, n_channels=32, kernel_size=9, strides=2, padding='valid')
+
+    # Layer 3: Capsule layer. Routing algorithm works here.
+    digitcaps = CapsuleLayer(num_capsule=n_class, dim_capsule=16, routings=routings, name='digitcaps')(primarycaps)
+
+    # Layer 4: This is an auxiliary layer to replace each capsule with its length. Just to match the true label's shape.
+    # If using tensorflow, this will not be necessary. :)
+    out_caps = Length(name='capsnet')(digitcaps)
+
+    # Decoder network.
+    y = layers.Input(shape=(n_class,))
+    masked_by_y = Mask()([digitcaps, y])  # The true label is used to mask the output of capsule layer. For training
+    masked = Mask()(digitcaps)  # Mask using the capsule with maximal length. For prediction
+
+    # Shared Decoder model in training and prediction
+    decoder = models.Sequential(name='decoder')
+    decoder.add(layers.Dense(512, activation='relu', input_dim=16 * n_class))
+    decoder.add(layers.Dense(1024, activation='relu'))
+    decoder.add(layers.Dense(np.prod(input_shape), activation='sigmoid'))
+    decoder.add(layers.Reshape(target_shape=input_shape, name='out_recon'))
+
+    # Models for training and evaluation (prediction)
+    train_model = models.Model([x, y], [out_caps, decoder(masked_by_y)])
+    eval_model = models.Model(x, [out_caps, decoder(masked)])
+
+    # manipulate model
+    noise = layers.Input(shape=(n_class, 16))
+    noised_digitcaps = layers.Add()([digitcaps, noise])
+    masked_noised_y = Mask()([noised_digitcaps, y])
+    manipulate_model = models.Model([x, y, noise], decoder(masked_noised_y))
+    return train_model, eval_model, manipulate_model
+
+
+def margin_loss(y_true, y_pred):
+    """
+    Margin loss for Eq.(4). When y_true[i, :] contains not just one `1`, this loss should work too. Not test it.
+    :param y_true: [None, n_classes]
+    :param y_pred: [None, num_capsule]
+    :return: a scalar loss value.
+    """
+    # return tf.reduce_mean(tf.square(y_pred))
+    L = y_true * tf.square(tf.maximum(0., 0.9 - y_pred)) + \
+        0.5 * (1 - y_true) * tf.square(tf.maximum(0., y_pred - 0.1))
+
+    return tf.reduce_mean(tf.reduce_sum(L, 1))
+
+
+def train(model,  # type: models.Model
+          data, args):
+    """
+    Training a CapsuleNet
+    :param model: the CapsuleNet model
+    :param data: a tuple containing training and testing data, like `((x_train, y_train), (x_test, y_test))`
+    :param args: arguments
+    :return: The trained model
+    """
+    # unpacking the data
+    (x_train, y_train), (x_test, y_test) = data
+
+    # callbacks
+    log = callbacks.CSVLogger(args.save_dir + '/log.csv')
+    checkpoint = callbacks.ModelCheckpoint(args.save_dir + '/weights-{epoch:02d}.h5', monitor='val_capsnet_acc',
+                                           save_best_only=True, save_weights_only=True, verbose=1)
+    lr_decay = callbacks.LearningRateScheduler(schedule=lambda epoch: args.lr * (args.lr_decay ** epoch))
+
+    # compile the model
+    model.compile(optimizer=optimizers.Adam(lr=args.lr),
+                  loss=[margin_loss, 'mse'],
+                  loss_weights=[1., args.lam_recon],
+                  metrics={'capsnet': 'accuracy'})
+
+    # Training without data augmentation:
+    model.fit([x_train, y_train], [y_train, x_train], batch_size=args.batch_size, epochs=args.epochs,
+              validation_data=[[x_test, y_test], [y_test, x_test]], callbacks=[log, checkpoint, lr_decay])
+
+    '''
+    # Begin: Training with data augmentation ---------------------------------------------------------------------#
+    def train_generator(x, y, batch_size, shift_fraction=0.):
+        train_datagen = ImageDataGenerator(width_shift_range=shift_fraction,
+                                           height_shift_range=shift_fraction)  # shift up to 2 pixel for MNIST
+        generator = train_datagen.flow(x, y, batch_size=batch_size)
+        while 1:
+            x_batch, y_batch = generator.next()
+            yield (x_batch, y_batch), (y_batch, x_batch)
+
+    # Training with data augmentation. If shift_fraction=0., no augmentation.
+    model.fit(train_generator(x_train, y_train, args.batch_size, args.shift_fraction),
+              steps_per_epoch=int(y_train.shape[0] / args.batch_size),
+              epochs=args.epochs,
+              validation_data=((x_test, y_test), (y_test, x_test)), batch_size=args.batch_size,
+              callbacks=[log, checkpoint, lr_decay])
+    # End: Training with data augmentation -----------------------------------------------------------------------#
+    '''
+
+    model.save_weights(args.save_dir + '/trained_model.h5')
+    print('Trained model saved to \'%s/trained_model.h5\'' % args.save_dir)
+
+    from utils import plot_log
+    plot_log(args.save_dir + '/log.csv', show=True)
+
+    return model
+
+
+def load_pcam():
+    # Load datasets
+    x_train, y_train = load_images_from_folder("/Users/vladbarbulescu/Desktop/CapsNet-Keras/pcam_mini/train+val/train")
+    x_test, y_test = load_images_from_folder("/Users/vladbarbulescu/Desktop/CapsNet-Keras/pcam_mini/train+val/valid")   
+
+    x_train = x_train.reshape(-1, 96, 96, 3).astype('float32') / 255.
+    x_test = x_test.reshape(-1, 96, 96, 3).astype('float32') / 255.
+
+    # Ensure shapes match
+    assert x_train.shape[0] == y_train.shape[0], "Mismatch in training data size"
+    assert x_test.shape[0] == y_test.shape[0], "Mismatch in validation data size"
+    
+    y_train = to_categorical(y_train.astype('float32'))
+    y_test = to_categorical(y_test.astype('float32'))
+
+    return (x_train, y_train), (x_test, y_test) 
+
+
+def load_images_from_folder(folder, target_size=(96, 96)):
+    from tensorflow.keras.preprocessing.image import load_img, img_to_array
+    images = []
+    labels = []
+        
+    for label in ["0", "1"]: 
+        class_path = os.path.join(folder, label)
+        for filename in os.listdir(class_path):
+            img_path = os.path.join(class_path, filename)
+            img = load_img(img_path, target_size=target_size)
+            img_array = img_to_array(img) 
+            images.append(img_array)
+            labels.append(int(label))  
+
+    return np.array(images), np.array(labels)
+
+
+if __name__ == "__main__":
+    import os
+    import argparse
+    from tensorflow.keras.preprocessing.image import ImageDataGenerator
+    from tensorflow.keras import callbacks
+
+    # setting the hyper parameters
+    parser = argparse.ArgumentParser(description="Capsule Network on MNIST.")
+    parser.add_argument('--epochs', default=50, type=int)
+    parser.add_argument('--batch_size', default=32, type=int)
+    parser.add_argument('--lr', default=0.001, type=float,
+                        help="Initial learning rate")
+    parser.add_argument('--lr_decay', default=0.9, type=float,
+                        help="The value multiplied by lr at each epoch. Set a larger value for larger epochs")
+    parser.add_argument('--lam_recon', default=0.392, type=float,
+                        help="The coefficient for the loss of decoder")
+    parser.add_argument('-r', '--routings', default=3, type=int,
+                        help="Number of iterations used in routing algorithm. should > 0")
+    parser.add_argument('--shift_fraction', default=0.1, type=float,
+                        help="Fraction of pixels to shift at most in each direction.")
+    parser.add_argument('--debug', action='store_true',
+                        help="Save weights by TensorBoard")
+    parser.add_argument('--save_dir', default='./result')
+    parser.add_argument('-t', '--testing', action='store_true',
+                        help="Test the trained model on testing dataset")
+    parser.add_argument('--digit', default=5, type=int,
+                        help="Digit to manipulate")
+    parser.add_argument('-w', '--weights', default=None,
+                        help="The path of the saved weights. Should be specified when testing")
+    args = parser.parse_args()
+    print(args)
+
+    if not os.path.exists(args.save_dir):
+        os.makedirs(args.save_dir)
+
+    # load data
+    (x_train, y_train), (x_test, y_test) = load_pcam()
+
+    # define model
+    model, eval_model, manipulate_model = CapsNet(input_shape=x_train.shape[1:],
+                                                  n_class=len(np.unique(np.argmax(y_train, 1))),
+                                                  routings=args.routings,
+                                                  batch_size=args.batch_size)
+    model.summary()
+
+    if args.weights is not None: # load weights
+        model.load_weights(args.weights)
+    if not args.testing:
+        train(model=model, data=((x_train, y_train), (x_test, y_test)), args=args)
